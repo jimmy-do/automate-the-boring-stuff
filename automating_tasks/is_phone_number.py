9afd6332ddc115d3b5010287d512028181fd1c30
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2024-05-08 22:58:01 -0500

    regex with strings

--- automating_tasks/is_phone_number.py
+++ automating_tasks/is_phone_number.py
@@ -1,4 +1,6 @@
 # This program checks a string for phone numbers.
+import re
+
 
 def is_phone_number(text):
     if len(text) != 12:
@@ -21,3 +23,12 @@ def is_phone_number(text):
 
 print(is_phone_number('415-555-4242'))  # Yes, a phone number!
 print(is_phone_number('Mochi Ice Cream'))  # Unfortunately, not a phone number!
+
+is_phone_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = is_phone_regex.search('Check this entire string for a US phone number 415-55-4242')
+
+if mo:
+    print('US phone number found: ' + mo.group())
+else:
+    print('No US phone number found')
+
