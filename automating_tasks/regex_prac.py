ddbffd068df971e37a593ffd225470ad2b00c4f6
jimmy-do <jimmydo07@yahoo.com>
2024-05-11 18:50:51 -0500

    more regex prac

--- automating_tasks/regex_prac.py
+++ automating_tasks/regex_prac.py
@@ -30,5 +30,44 @@ print(mo.group())  # Will still return 'Batman' since matching is optional
 
 batRegex = re.compile('Bat(wo)+man')
 mo = batRegex.search('Batman')
-print(mo is None)  # should equate to True since matching in this case was not optional
+print(mo is None)  # Should equate to True since matching in this case was not optional
 
+
+# The curly braces {} will check for specified number of occurrences
+
+haRegex = re.compile(r'(Ha){3}') # Checks for 3 occurrences 
+mo = haRegex.search('HaHaHa')
+print(mo.group())
+
+greedyHaRegex = re.compile(r'(Ha){2,5}') # Checks for 2 to 5 occurrences -> will always match longest string
+mo = greedyHaRegex.search('HaHaHa')
+print(mo.group())
+
+lazyHaRegex = re.compile(r'(Ha){2,5}?') # Lazy regex matching; will match shortest string
+mo = lazyHaRegex.search('HaHaHa')
+print(mo.group())
+
+# The findall() method will return occurrences found in a list or tuple (if there are groups) format
+
+phoneNumberRegex = re.compile('\d\d\d-\d\d\d-\d\d\d\d')
+print(phoneNumberRegex.findall('Home: 555-123-4567 Cell: 888-123-6789'))
+
+# Character cases will help you specify what you're looking for
+
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans,'
+                  ' 6 geese, 5 rings, 4 bird3s, 3 hens, 2 doves, 1 partridge'))
+
+# Making your own character classes
+
+vowelRegex = re.compile('[aeiouAEIOU]')
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+vowelRegex = re.compile('[^aeiouAEIOU]') # Adding the ^ operator will exclude these characters
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+vowelRegex = re.compile('[a-z]')
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+vowelRegex = re.compile('[a-zA-Z0-9]') # Regex object will include all lower and upper case + digits
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
