3862950b352e80008b09af8d68cc0fcb003545d8
jimmy-do <jimmydo07@yahoo.com>
2024-05-13 16:19:15 -0500

    more regex prac

--- automating_tasks/regex_prac.py
+++ automating_tasks/regex_prac.py
@@ -1,5 +1,26 @@
 import re
 
+"""
+
+• The ? matches zero or one of the preceding group.
+• The * matches zero or more of the preceding group.
+• The + matches one or more of the preceding group.
+• The {n} matches exactly n of the preceding group.
+• The {n,} matches n or more of the preceding group.
+• The {,m} matches 0 to m of the preceding group.
+• The {n,m} matches at least n and at most m of the preceding group.
+• {n,m}? or *? or +? performs a non-greedy match of the preceding group.
+• ^spam means the string must begin with spam.
+• spam$ means the string must end with spam.
+• The . matches any character, except newline characters.
+• \d, \w, and \s match a digit, word, or space character, respectively.
+• \D, \W, and \S match anything except a digit, word, or space character,
+respectively.
+• [abc] matches any character between the brackets (such as a, b, or c).
+• [^abc] matches any character that isn’t between the brackets.
+
+"""
+
 # The | operator will include either result. It will match the first occurrence:
 
 batRegex = re.compile(r'Batman|Batwoman')
@@ -106,23 +127,7 @@ newlineRegex = re.compile(r'.*', re.DOTALL)
 print(newlineRegex.search('Serve the public trust.\nProtect the innocent.'
                        '\nUphold the law.').group())
 
-"""
+# Ignoring casing with re.I
 
-• The ? matches zero or one of the preceding group.
-• The * matches zero or more of the preceding group.
-• The + matches one or more of the preceding group.
-• The {n} matches exactly n of the preceding group.
-• The {n,} matches n or more of the preceding group.
-• The {,m} matches 0 to m of the preceding group.
-• The {n,m} matches at least n and at most m of the preceding group.
-• {n,m}? or *? or +? performs a non-greedy match of the preceding group.
-• ^spam means the string must begin with spam.
-• spam$ means the string must end with spam.
-• The . matches any character, except newline characters.
-• \d, \w, and \s match a digit, word, or space character, respectively.
-• \D, \W, and \S match anything except a digit, word, or space character,
-respectively.
-• [abc] matches any character between the brackets (such as a, b, or c).
-• [^abc] matches any character that isn’t between the brackets.
-
-"""
+robocop = re.compile('robocop', re.IGNORECASE)
+print(robocop.search('ROBOCOP protects the innocent.').group())
